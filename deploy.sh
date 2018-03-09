b2f9036e70362eeaa0e62971d1a46826f5490cbd
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-03-09 13:02:01 +0000

    ACP_MIGRATION: update for deployment to prod (pr namespace)

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
-export KUBE_TOKEN=${KUBE_TOKEN}
+export VERSION=0.1.0
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_FS_PR drone secret"
+    export KUBE_TOKEN=${PTTG_FS_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_FS_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_FS_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_FS_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_FS_DEV}
+    fi
 fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
 
 cd kd
 kd --insecure-skip-tls-verify \
